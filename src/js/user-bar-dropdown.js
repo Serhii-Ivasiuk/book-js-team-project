ef1fd9efede7b7030dd294670a172e9e20f40540
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-12 19:55:37 +0300

    update dropdown

--- src/js/user-bar-dropdown.js
+++ src/js/user-bar-dropdown.js
@@ -1,5 +1,5 @@
 const refs = {
-  dropdown: document.querySelector('.js-dropdown'),
+  userBar: document.querySelector('.js-user-bar'),
   userBarBtn: document.querySelector('.js-user-bar-btn'),
   logOutBtn: document.querySelector('.js-log-out-btn'),
 };
@@ -8,11 +8,11 @@ refs.userBarBtn.addEventListener('click', handleUserBarBtnClick);
 refs.logOutBtn.addEventListener('click', handleLogOutBtnClick);
 
 function handleUserBarBtnClick() {
-  refs.dropdown.classList.toggle('is-active');
+  refs.userBar.classList.toggle('is-active');
 }
 
 function handleLogOutBtnClick() {
-  refs.dropdown.classList.remove('is-active');
+  refs.userBar.classList.remove('is-active');
   /*
   ===============================================================
   тут нужно будет добавить логику выхода из аккаунта пользователя
