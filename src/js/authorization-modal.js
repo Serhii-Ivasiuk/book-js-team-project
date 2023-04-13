0d3f8149f138267bc667054fba28522274c11bff
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-13 18:21:02 +0300

    update open/close modal

--- src/js/authorization-modal.js
+++ src/js/authorization-modal.js
@@ -1,25 +1,66 @@
+// const refs = {
+//   openModalAuthorization: document.querySelector('[data-modal-open]'),
+//   closeModalAuthorization: document.querySelector('[data-modal-close]'),
+//   modal: document.querySelector('[data-modal]'),
+//   backdrop: document.querySelector('.authorization__bacdrop'),
+// };
+
+// refs.openModalAuthorization.addEventListener('click', e => {
+//   toggleModal();
+// });
+
+// refs.closeModalAuthorization.addEventListener('click', e => {
+//   toggleModal();
+// });
+
+// refs.modal.addEventListener('click', e => {
+//   if (e.target !== refs.backdrop) {
+//     return;
+//   }
+//   toggleModal();
+// });
+
+// function toggleModal() {
+//   refs.modal.classList.toggle('is-hidden');
+// }
+
+const ESC_KEY_CODE = 'Escape';
+
 const refs = {
-  openModalAuthorization: document.querySelector('[data-modal-open]'),
-  closeModalAuthorization: document.querySelector('[data-modal-close]'),
-  modal: document.querySelector('[data-modal]'),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
   backdrop: document.querySelector('.authorization__bacdrop'),
+  modal: document.querySelector('[data-modal]'),
 };
 
-refs.openModalAuthorization.addEventListener('click', e => {
-  toggleModal();
-});
+refs.openModalBtn.addEventListener('click', openModal);
 
-refs.closeModalAuthorization.addEventListener('click', e => {
-  toggleModal();
-});
-
-refs.modal.addEventListener('click', e => {
-  if (e.target !== refs.backdrop) {
-    return;
+function onBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    closeModel();
   }
-  toggleModal();
-});
+}
 
-function toggleModal() {
-  refs.modal.classList.toggle('is-hidden');
+function openModal() {
+  refs.modal.classList.remove('is-hidden');
+
+  // add listeners to closeModalBtn, backdrop, keydown(escape)
+  refs.closeModalBtn.addEventListener('click', closeModel);
+  refs.backdrop.addEventListener('click', onBackdropClick);
+  window.addEventListener('keydown', onEscapeKeydown);
+}
+
+function closeModel() {
+  refs.modal.classList.add('is-hidden');
+
+  // remove listiners from closeModalBtn, backdrop, keydown(escape)
+  refs.closeModalBtn.removeEventListener('click', closeModel);
+  refs.backdrop.removeEventListener('click', onBackdropClick);
+  window.removeEventListener('keydown', onEscapeKeydown);
+}
+
+function onEscapeKeydown(e) {
+  if (e.key === ESC_KEY_CODE) {
+    closeModel();
+  }
 }
