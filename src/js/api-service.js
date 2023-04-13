4e46a466cab51965fdd06c72ddd37fb3d6b677e1
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-13 14:56:17 +0300

    Update api-service.js

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -14,7 +14,7 @@ export async function getCategoryList() {
 }
 
 //for example use function
-getCategoryList().then(data => console.log(data));
+// getCategoryList().then(data => console.log(data));
 
 export async function getBooksByCategory(category) {
   const { data } = await axios.get(BOOKS_BY_CATEGORY, {
