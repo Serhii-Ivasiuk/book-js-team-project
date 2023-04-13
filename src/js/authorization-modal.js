c0be312deff4cfe6f9bfdd151100c9dca6316931
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-13 18:32:54 +0300

    Update authorization-modal.js

--- src/js/authorization-modal.js
+++ src/js/authorization-modal.js
@@ -1,29 +1,3 @@
-// const refs = {
-//   openModalAuthorization: document.querySelector('[data-modal-open]'),
-//   closeModalAuthorization: document.querySelector('[data-modal-close]'),
-//   modal: document.querySelector('[data-modal]'),
-//   backdrop: document.querySelector('.authorization__bacdrop'),
-// };
-
-// refs.openModalAuthorization.addEventListener('click', e => {
-//   toggleModal();
-// });
-
-// refs.closeModalAuthorization.addEventListener('click', e => {
-//   toggleModal();
-// });
-
-// refs.modal.addEventListener('click', e => {
-//   if (e.target !== refs.backdrop) {
-//     return;
-//   }
-//   toggleModal();
-// });
-
-// function toggleModal() {
-//   refs.modal.classList.toggle('is-hidden');
-// }
-
 const ESC_KEY_CODE = 'Escape';
 
 const refs = {
