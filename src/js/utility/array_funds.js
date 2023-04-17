62ae37443d37c168344a820ec4e5b06f4af70514
Petro Bilous <111942988+BilousPM@users.noreply.github.com>
2023-04-17 15:51:21 -0600

    delete folder js_support

---
+++ src/js/utility/array_funds.js
@@ -0,0 +1,76 @@
+import actionAgainst from '../../images/charity/action-against@1x.png';
+import actionAgainst2 from '../../images/charity/action-against@2x.png';
+import internationalMedical from '../../images/charity/international-medical@1x.png';
+import internationalMedical2 from '../../images/charity/international-medical@2x.png';
+import medicinsSans from '../../images/charity/medicins-sans@1x.png';
+import medicinsSans2 from '../../images/charity/medicins-sans@2x.png';
+import projectHope from '../../images/charity/project-hope@1x.png';
+import projectHope2 from '../../images/charity/project-hope@2x.png';
+import razom from '../../images/charity/razom@1x.png';
+import razom2 from '../../images/charity/razom@2x.png';
+import saveTheChildren from '../../images/charity/save-the-children@1x.png';
+import saveTheChildren2 from '../../images/charity/save-the-children@2x.png';
+import sergiyPrytula from '../../images/charity/sergiy-prytula@1x.png';
+import sergiyPrytula2 from '../../images/charity/sergiy-prytula@2x.png';
+import united24 from '../../images/charity/united24@1x.png';
+import united242 from '../../images/charity/united24@2x.png';
+import worldVision from '../../images/charity/world-vision@1x.png';
+import worldVision2 from '../../images/charity/world-vision@2x.png';
+
+export const arrayFunds = [
+  {
+    title: 'Save the Children',
+    url:
+      'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: saveTheChildren,
+    img_2x: saveTheChildren2
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: projectHope,
+    img_2x: projectHope2
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: united24,
+    img_2x: united242
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: internationalMedical,
+    img_2x: internationalMedical2
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: medicinsSans,
+    img_2x: medicinsSans2
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: razom,
+    img_2x: razom2
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: actionAgainst,
+    img_2x: actionAgainst2
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: worldVision,
+    img_2x: worldVision2
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: sergiyPrytula,
+    img_2x: sergiyPrytula2
+  }
+];
