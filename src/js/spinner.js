b94ed6e9408e3d4cd7dc0646874d2b7b582c04e6
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-04-19 22:25:21 +0300

    return changes

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -3,11 +3,44 @@ import { refs } from './utility/refs';
 export function showSpinner() {
   refs.spinner.classList.contains('done') &&
     refs.spinner.classList.remove('done');
+  refs.spinner.insertAdjacentHTML('afterbegin', SpinnerTemplate());
   document.body.style.overflow = 'hidden';
 }
 
 export function hideSpinner() {
   !refs.spinner.classList.contains('done') &&
     refs.spinner.classList.add('done');
+  refs.spinner.replaceChildren();
   document.body.style.overflow = '';
+}
+
+export function SpinnerTemplate() {
+  return `
+  <div class="book">
+    <div class="inner">
+      <div class="left"></div>
+      <div class="middle"></div>
+      <div class="right"></div>
+    </div>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>`;
 }
